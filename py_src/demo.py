f12a8b3d7994192ea8ba8677377f6431cb3daac5
Navid Salehnamadi <navid.saleh.n@gmail.com>
2022-02-03 16:39:57 -0800

    Add annotation for elements

--- py_src/demo.py
+++ py_src/demo.py
@@ -3,29 +3,42 @@ import asyncio
 import logging
 import json
 from ppadb.client_async import ClientAsync as AdbClient
-from padb_utils import ParallelADBLogger
-from latte_utils import talkback_tree_nodes
-
+from padb_utils import ParallelADBLogger, save_screenshot
+from latte_utils import talkback_tree_nodes, latte_capture_layout
+from GUI_utils import get_actions_from_layout
+from utils import annotate_elements
 
 logger = logging.getLogger(__name__)
 
 
-async def execute_latte_command(command: str):
+async def execute_latte_command(command: str, extra: str):
     client = AdbClient(host="127.0.0.1", port=5037)
     device = await client.device("emulator-5554")
     padb_logger = ParallelADBLogger(device)
     if command == "tb_a11y_tree":
-        windows_info, bm_logs = await talkback_tree_nodes(padb_logger, verbose=False)
-        logger.info(f"Windows Info: {json.dumps(windows_info, indent=4, sort_keys=True)}")
+        windows_info, bm_logs = await talkback_tree_nodes(padb_logger, verbose=True)
+        logger.info(f"Windows Info: {json.dumps(windows_info, indent=4)}")
         logger.info(f"Latte Logs: {bm_logs}")
+    if command == "capture_layout":
+        log, layout = await padb_logger.execute_async_with_log(latte_capture_layout())
+        logger.info(layout)
+    if command == "get_actions":  # The extra is the output path
+        await save_screenshot(device, extra)
+        log, layout = await padb_logger.execute_async_with_log(latte_capture_layout())
+        if "Problem with XML" in layout:
+            logger.error(layout)
+            logger.error("Logs: " + log)
+            return
+        actions = get_actions_from_layout(layout)
+        annotate_elements(extra, extra, actions, outline=(255, 0, 255), width=15, scale=5)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--command', type=str, help='The command sending to Latte')
+    parser.add_argument('--extra', type=str, default="", help='The extra information sent to Latte')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG)
     if args.command:
-        asyncio.run(execute_latte_command(args.command))
-
+        asyncio.run(execute_latte_command(args.command, args.extra))
--- py_src/utils.py
+++ py_src/utils.py
@@ -1,8 +1,19 @@
 import logging
+from typing import List, Union, Tuple
+from pathlib import Path
 from PIL import Image, ImageDraw
 
-
 logger = logging.getLogger(__name__)
+
+
+def convert_bounds(bounds_str: str) -> Tuple[int, ...]:
+    return tuple(
+        [int(x) for x in
+         bounds_str
+             .replace('][', ',')
+             .replace(']', '')
+             .replace('[', '')
+             .split(',')])
 
 
 def dashed_line(draw, bound, width=1, fill=None, scale=5):
@@ -27,12 +38,64 @@ def dashed_rectangle(draw, bound, fill=None, outline=None, width=1, scale=5):
     dashed_line(draw, (bound[2], bound[1], bound[0], bound[1]), width=width, fill=outline, scale=scale)
 
 
-def annotate_rectangle(source_img, target_img, bound, outline=None, width=10, scale=5):
+def annotate_rectangle(source_img,
+                       target_img,
+                       bounds: List[Tuple],
+                       outline: Union[List, Tuple] = None,
+                       width: Union[List, int] = 10,
+                       scale: Union[List, int] = 5):
+    if isinstance(outline, list):
+        if len(outline) != len(bounds):
+            logger.error(
+                f"The bounds and outline should be the same size. bounds: {len(bounds)}, outline: {len(outline)}")
+            return
+    else:
+        outline = [outline] * len(bounds)
+
+    if isinstance(width, list):
+        if len(width) != len(bounds):
+            logger.error(f"The bounds and width should be the same size. bounds: {len(width)}, outline: {len(outline)}")
+            return
+    else:
+        width = [width] * len(bounds)
+
+    if isinstance(scale, list):
+        if len(scale) != len(bounds):
+            logger.error(f"The bounds and scale should be the same size. bounds: {len(scale)}, outline: {len(outline)}")
+            return
+    else:
+        scale = [scale] * len(bounds)
+
     try:
         im = Image.open(source_img)
         draw = ImageDraw.Draw(im)
         # draw.rectangle(reg_result.bound, fill=(255, 255, 0, 20), outline=(100, 100, 100))
-        dashed_rectangle(draw, bound, outline=outline, width=width, scale=scale)
+        for bound, o, w, s in zip(bounds, outline, width, scale):
+            new_bound = (min(bound[0], bound[2]),
+                         min(bound[1], bound[3]),
+                         max(bound[0], bound[2]),
+                         max(bound[1], bound[3]))
+            dashed_rectangle(draw, new_bound, outline=o, width=w, scale=s)
         im.save(target_img, quality=95)
     except Exception as e:
         logger.error(f"A problem with image annotation, Exception: {e}")
+
+
+def annotate_elements(source_img: Union[str, Path],
+                      target_img: Union[str, Path],
+                      elements: List,
+                      outline: Tuple = None,
+                      width: int = 10,
+                      scale: int = 5):
+    bounds = []
+    for element in elements:
+        if not element['bounds'] or element['bounds'] == 'null':
+            logger.error(f"The bounds of element {element} is empty!")
+            continue
+        bounds.append(convert_bounds(element['bounds']))
+    annotate_rectangle(source_img,
+                       target_img,
+                       bounds,
+                       outline=outline,
+                       width=width,
+                       scale=scale)
