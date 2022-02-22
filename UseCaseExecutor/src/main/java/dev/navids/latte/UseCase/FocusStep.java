af0581c4274f05baa31254a44d79d35bed60cef9
Navid Salehnamadi <navid.saleh.n@gmail.com>
2022-02-22 13:45:02 -0800

    add missing file

---
+++ UseCaseExecutor/src/main/java/dev/navids/latte/UseCase/FocusStep.java
@@ -0,0 +1,23 @@
+package dev.navids.latte.UseCase;
+
+import android.util.Log;
+
+import org.json.simple.JSONObject;
+
+import dev.navids.latte.LatteService;
+
+public class FocusStep extends LocatableStep {
+    FocusStep(JSONObject stepJson) {
+        super(stepJson);
+        Log.i(LatteService.TAG, "Focusable Step: " + this.getTargetWidgetInfo());
+    }
+    public static boolean isFocusStep(String action){
+        return action.equals("focus");
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("FocusStep{State=%s,WidgetTarget=%s}", getState(), getTargetWidgetInfo());
+    }
+}
