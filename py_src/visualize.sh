ef1efb12eda37bce22cbc50571de7c7ebec8bc59
Forough <forough@Foroughs-MacBook-Pro.local>
2022-03-12 12:09:14 -0800

    Add more scripts

--- py_src/visualize.sh
+++ py_src/visualize.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 DEV=${1-"nodev"}
+host="127.0.0.1"
 if [[ $DEV == "dev" ]]; then
   export FLASK_ENV=development
+elif [[ $DEV == "prod" ]]; then
+  host="0.0.0.0"
 fi
-python -m flask run
+
+python -m flask run --host="$host"
