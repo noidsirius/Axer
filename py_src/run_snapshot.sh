5e4f3be0fa3a223d905f30d21bb3b9164f03111d
Navid Salehnamadi <navid.saleh.n@gmail.com>
2022-03-16 00:51:55 -0700

    f

---
+++ py_src/run_snapshot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SNAPSHOT_TIMEOUT=900
+SNAPSHOT=$1
+RESULT_PATH=${2:-$(realpath ../dev_results)}
+RESULT_PATH=$(realpath "$RESULT_PATH")
+DIRECTIONAL_ACTION_LIMIT="--dir-action-limit 5"
+POINT_ACTION_LIMIT="--point-action-limit 5"
+OVERSIGHT="--oversight"
+APP_NAME=${SNAPSHOT%%.S_*}
+echo "Snapshot $SNAPSHOT in App $APP_NAME"
+gtimeout $SNAPSHOT_TIMEOUT python main.py --app-name "$APP_NAME" --output-path "$RESULT_PATH" --snapshot "$SNAPSHOT" --debug $DIRECTIONAL_ACTION_LIMIT $POINT_ACTION_LIMIT $OVERSIGHT
+TMP_SNAPSHOT=$SNAPSHOT"_TMP"
+adb emu avd snapshot delete "$TMP_SNAPSHOT"
+rm -rf ~/.android/avd/Pixel_Stoat.avd/snapshots/"$TMP_SNAPSHOT"
+echo "REMOVE $TMP_SNAPSHOT"
+
