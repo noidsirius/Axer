1a4d0b09bacb63dd7ada00620796a72eddd3715d
Navid Salehnamadi <navid.saleh.n@gmail.com>
2022-06-30 23:53:20 -0700

    Add args to client

--- py_src/client.py
+++ py_src/client.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import random
+import sys
 
 import websockets
 import json
@@ -9,25 +10,32 @@ import json
 from command import InfoCommand, create_command_from_dict
 
 
-async def hello():
+async def recorder_client():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? (Options: ['RECORDER', 'TOCUH', 'TB', 'API']) ")
-        message = {'name': name}
+        message = {'name': 'RECORDER'}
         await websocket.send(json.dumps(message))
-        if name == 'RECORDER':
-            for i in range(5):
-                command = InfoCommand(question=f"Command {i}")
-                await websocket.send(command.toJSONStr())
-                await asyncio.sleep(1 + random.random()*3)
-        else:
-            while True:
-                command_str = await websocket.recv()
-                command_json = json.loads(command_str)
-                print(f"Received command '{command_str}'")
-                command = create_command_from_dict(command_json)
+        for i in range(5):
+            command = InfoCommand(question=f"Command {i}")
+            await websocket.send(command.toJSONStr())
+            await asyncio.sleep(1 + random.random()*3)
 
+
+async def proxy_user_client(proxy_user_name: str):
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        message = {'name': proxy_user_name}
+        await websocket.send(json.dumps(message))
+        while True:
+            command_str = await websocket.recv()
+            command_json = json.loads(command_str)
+            print(f"Received command '{command_str}'")
+            command = create_command_from_dict(command_json)
 
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    name = sys.argv[1]
+    if name == 'RECORDER':
+        asyncio.run(recorder_client())
+    else:
+        asyncio.run(proxy_user_client(name))
