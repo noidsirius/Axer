f31b4d569b7cf1d136fe461309b654d09514323a
Navid Salehnamadi <navid.saleh.n@gmail.com>
2022-07-27 16:42:44 -0700

    Improve unittest

--- py_src/test/test_node.py
+++ py_src/test/test_node.py
@@ -5,6 +5,7 @@ import json
 
 node1_json_str = '{"a11y_actions": ["4", "8", "64", "16908342"], "action": "click", "bounds": [540, 214, 540, 281], "checkable": false, "checked": false, "class_name": "android.widget.TextView", "clickable": false, "clickable_span": false, "content_desc": "my_content", "context_clickable": false, "covered": false, "drawing_order": 1, "enabled": true, "focusable": false, "focused": false, "important_for_accessibility": true, "index": 0, "invalid": false, "is_ad": false, "located_by": "xpath", "long_clickable": false, "naf": false, "pkg_name": "au.gov.nsw.newcastle.app.android", "resource_id": "my_res", "skip": false, "text": "my_text", "visible": false, "xpath": "/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.TextView"}'
 
+
 # TODO: Add a node which is captured from Latte
 
 
@@ -61,3 +62,17 @@ class TestNode(unittest.TestCase):
         self.assertTrue(node1.practically_equal(node2))
         node2.text = "something else"
         self.assertFalse(node1.practically_equal(node2))
+
+    def test_bounds(self):
+        node1 = Node(bounds=(1, 2, 3, 4))
+        self.assertEqual((1, 2, 3, 4), node1.bounds)
+        self.assertTrue(node1.is_valid_bounds())
+        node2 = Node.createNodeFromDict({"bounds": "[10,20][110,220]"})
+        self.assertEqual((10, 20, 110, 220), node2.bounds)
+        self.assertTrue(node2.is_valid_bounds())
+        self.assertFalse(Node(bounds=(10, 10, 5, 15)).is_valid_bounds())
+        self.assertFalse(Node(bounds=(10, 10, 15, 5)).is_valid_bounds())
+        self.assertFalse(Node(bounds=(10, 10, 5, 5)).is_valid_bounds())
+        normalized_bounds = node2.get_normalized_bounds((0, 10, 500, 410))
+        self.assertEqual((10/500, 10/400, 110/500, 210/400), normalized_bounds)
+
