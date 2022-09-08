8bd5b818295f752537c3506a67d64c68ba001d97
Navid Salehnamadi <navid.saleh.n@gmail.com>
2022-09-07 17:19:38 -0700

    Remove utils

--- utils/src/main/java/com/google/android/accessibility/utils/ExperimentalUtils.java
+++
@@ -1,7 +0,0 @@
-package com.google.android.accessibility.utils;
-
-
-/** Stubs of functions from the unreleased experimental android version. */
-public final class ExperimentalUtils {
-  private ExperimentalUtils() {}
-}
